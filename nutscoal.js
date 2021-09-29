b9d1df8f88a819399026fbf058f7a0df99aa0c99
benmcwilliams <66636868+benmcwilliams@users.noreply.github.com>
2021-09-29 11:49:32 +0200

    more data

---
+++ nutscoal.js
@@ -0,0 +1,617 @@
+nutscoal = {
+   "type": "FeatureCollection",
+   "features": [
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 6.829,50.762 ]
+    },
+    "properties": {
+    "FIELD1":5,
+    "nuts2":"DEA2",
+    "country":"Germany",
+    "production":60,
+    "id":"DEA2",
+    "na":"Köln",
+    "ar":7361524424,
+    "bubble_size":3
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 19.005,50.233 ]
+    },
+    "properties": {
+    "FIELD1":16,
+    "nuts2":"PL22",
+    "country":"Poland",
+    "production":59,
+    "id":"PL22",
+    "na":"Slaskie",
+    "ar":12332572991,
+    "bubble_size":3
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 19.367,51.668 ]
+    },
+    "properties": {
+    "FIELD1":19,
+    "nuts2":"PL11",
+    "country":"Poland",
+    "production":42.1,
+    "id":"PL11",
+    "na":"Lódzkie",
+    "ar":18218097417,
+    "bubble_size":3
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 13.372,50.352 ]
+    },
+    "properties": {
+    "FIELD1":3,
+    "nuts2":"CZ04",
+    "country":"Czech Republic",
+    "production":38.1,
+    "id":"CZ04",
+    "na":"Severozápad",
+    "ar":8653474625,
+    "bubble_size":3
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 21.486,40.4 ]
+    },
+    "properties": {
+    "FIELD1":12,
+    "nuts2":"EL53",
+    "country":"Greece",
+    "production":37.9,
+    "id":"EL53",
+    "na":"Dytiki Makedonia",
+    "ar":9463442750,
+    "bubble_size":3
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 6.629,51.412 ]
+    },
+    "properties": {
+    "FIELD1":6,
+    "nuts2":"DEA1",
+    "country":"Germany",
+    "production":35,
+    "id":"DEA1",
+    "na":"Düsseldorf",
+    "ar":5293498969,
+    "bubble_size":3
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 13.738,52.07 ]
+    },
+    "properties": {
+    "FIELD1":8,
+    "nuts2":"DE40",
+    "country":"Germany",
+    "production":34,
+    "id":"DE40",
+    "na":"Brandenburg",
+    "ar":29654788408,
+    "bubble_size":3
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 26.765,42.437 ]
+    },
+    "properties": {
+    "FIELD1":0,
+    "nuts2":"BG34",
+    "country":"Bulgaria",
+    "production":32.6,
+    "id":"BG34",
+    "na":"Yugoiztochen",
+    "ar":19804047526,
+    "bubble_size":3
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 14.112,51.176 ]
+    },
+    "properties": {
+    "FIELD1":9,
+    "nuts2":"DED2",
+    "country":"Germany",
+    "production":28,
+    "id":"DED2",
+    "na":"Dresden",
+    "ar":7944354693,
+    "bubble_size":3
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 23.451,44.572 ]
+    },
+    "properties": {
+    "FIELD1":22,
+    "nuts2":"RO41",
+    "country":"Romania",
+    "production":24,
+    "id":"RO41",
+    "na":"Sud-Vest Oltenia",
+    "ar":29213204438,
+    "bubble_size":3
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 17.441,52.191 ]
+    },
+    "properties": {
+    "FIELD1":21,
+    "nuts2":"PL41",
+    "country":"Poland",
+    "production":13.7,
+    "id":"PL41",
+    "na":"Wielkopolskie",
+    "ar":29822796639,
+    "bubble_size":3
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 12.682,51.322 ]
+    },
+    "properties": {
+    "FIELD1":11,
+    "nuts2":"DED5",
+    "country":"Germany",
+    "production":10,
+    "id":"DED5",
+    "na":"Leipzig",
+    "ar":3977248407,
+    "bubble_size":3
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 11.874,51.983 ]
+    },
+    "properties": {
+    "FIELD1":10,
+    "nuts2":"DEE0",
+    "country":"Germany",
+    "production":9.3,
+    "id":"DEE0",
+    "na":"Sachsen-Anhalt",
+    "ar":20546870225,
+    "bubble_size":2
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 20.529,48.017 ]
+    },
+    "properties": {
+    "FIELD1":14,
+    "nuts2":"HU31",
+    "country":"Hungary",
+    "production":9.3,
+    "id":"HU31",
+    "na":"Észak-Magyarország",
+    "ar":13428343597,
+    "bubble_size":2
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 22.881,51.183 ]
+    },
+    "properties": {
+    "FIELD1":18,
+    "nuts2":"PL31",
+    "country":"Poland",
+    "production":8.5,
+    "id":"PL31",
+    "na":"Lubelskie",
+    "ar":25114737744,
+    "bubble_size":2
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 18.003,49.761 ]
+    },
+    "properties": {
+    "FIELD1":2,
+    "nuts2":"CZ08",
+    "country":"Czech Republic",
+    "production":8.3,
+    "id":"CZ08",
+    "na":"Moravskoslezsko",
+    "ar":5423798871,
+    "bubble_size":2
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 22.379,37.393 ]
+    },
+    "properties": {
+    "FIELD1":13,
+    "nuts2":"EL65",
+    "country":"Greece",
+    "production":8.1,
+    "id":"EL65",
+    "na":"Peloponnisos",
+    "ar":15512707271,
+    "bubble_size":2
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 16.547,51.061 ]
+    },
+    "properties": {
+    "FIELD1":20,
+    "nuts2":"PL51",
+    "country":"Poland",
+    "production":7.3,
+    "id":"PL51",
+    "na":"Dolnoslaskie",
+    "ar":19944719812,
+    "bubble_size":2
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 7.39,51.937 ]
+    },
+    "properties": {
+    "FIELD1":4,
+    "nuts2":"DEA3",
+    "country":"Germany",
+    "production":6.7,
+    "id":"DEA3",
+    "na":"Münster",
+    "ar":6919856164,
+    "bubble_size":2
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 20.252,49.912 ]
+    },
+    "properties": {
+    "FIELD1":17,
+    "nuts2":"PL21",
+    "country":"Poland",
+    "production":4.7,
+    "id":"PL21",
+    "na":"Malopolskie",
+    "ar":15181116082,
+    "bubble_size":2
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 15.291,46.112 ]
+    },
+    "properties": {
+    "FIELD1":25,
+    "nuts2":"SI03",
+    "country":"Slovenia",
+    "production":3.2,
+    "id":"SI03",
+    "na":"Vzhodna Slovenija",
+    "ar":12428372219,
+    "bubble_size":2
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -2.019,55.317 ]
+    },
+    "properties": {
+    "FIELD1":31,
+    "nuts2":"UKC2",
+    "country":"United Kingdom",
+    "production":3.15,
+    "id":"UKC2",
+    "na":"Northumberland and Tyne and Wear",
+    "ar":5578580587,
+    "bubble_size":2
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 23.422,42.278 ]
+    },
+    "properties": {
+    "FIELD1":1,
+    "nuts2":"BG41",
+    "country":"Bulgaria",
+    "production":3,
+    "id":"BG41",
+    "na":"Yugozapaden",
+    "ar":20303549527,
+    "bubble_size":2
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 18.003,48.385 ]
+    },
+    "properties": {
+    "FIELD1":24,
+    "nuts2":"SK02",
+    "country":"Slovakia",
+    "production":1.8,
+    "id":"SK02",
+    "na":"Západné Slovensko",
+    "ar":14991208022,
+    "bubble_size":1
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -4.046,52.051 ]
+    },
+    "properties": {
+    "FIELD1":40,
+    "nuts2":"UKL1",
+    "country":"United Kingdom",
+    "production":1.6,
+    "id":"UKL1",
+    "na":"West Wales and The Valleys",
+    "ar":13161071637,
+    "bubble_size":1
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -1.389,54.215 ]
+    },
+    "properties": {
+    "FIELD1":34,
+    "nuts2":"UKE2",
+    "country":"United Kingdom",
+    "production":1.45,
+    "id":"UKE2",
+    "na":"North Yorkshire",
+    "ar":8320434045,
+    "bubble_size":1
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 21.932,45.633 ]
+    },
+    "properties": {
+    "FIELD1":23,
+    "nuts2":"RO42",
+    "country":"Romania",
+    "production":1.3,
+    "id":"RO42",
+    "na":"Vest",
+    "ar":32022049414,
+    "bubble_size":1
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -0.701,41.464 ]
+    },
+    "properties": {
+    "FIELD1":26,
+    "nuts2":"ES24",
+    "country":"Spain",
+    "production":1.3,
+    "id":"ES24",
+    "na":"Aragón",
+    "ar":47722121755,
+    "bubble_size":1
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -5.846,43.312 ]
+    },
+    "properties": {
+    "FIELD1":27,
+    "nuts2":"ES12",
+    "country":"Spain",
+    "production":1.2,
+    "id":"ES12",
+    "na":"Principado de Asturias",
+    "ar":10599692270,
+    "bubble_size":1
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -4.022,55.39 ]
+    },
+    "properties": {
+    "FIELD1":36,
+    "nuts2":"UKM3",
+    "country":"United Kingdom",
+    "production":1.05,
+    "id":"UKM3",
+    "na":"South Western Scotland",
+    "ar":13133392087,
+    "bubble_size":1
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -3.383,52.147 ]
+    },
+    "properties": {
+    "FIELD1":39,
+    "nuts2":"UKL2",
+    "country":"United Kingdom",
+    "production":0.8,
+    "id":"UKL2",
+    "na":"East Wales",
+    "ar":7663626244,
+    "bubble_size":1
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -4.828,41.823 ]
+    },
+    "properties": {
+    "FIELD1":28,
+    "nuts2":"ES41",
+    "country":"Spain",
+    "production":0.36,
+    "id":"ES41",
+    "na":"Castilla y León",
+    "ar":94222069757,
+    "bubble_size":1
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -1.35,53.089 ]
+    },
+    "properties": {
+    "FIELD1":35,
+    "nuts2":"UKF1",
+    "country":"United Kingdom",
+    "production":0.35,
+    "id":"UKF1",
+    "na":"Derbyshire and Nottinghamshire",
+    "ar":4794997155,
+    "bubble_size":1
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -3.161,39.254 ]
+    },
+    "properties": {
+    "FIELD1":29,
+    "nuts2":"ES42",
+    "country":"Spain",
+    "production":0.2,
+    "id":"ES42",
+    "na":"Castilla-La Mancha",
+    "ar":79454042942,
+    "bubble_size":1
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -1.344,53.451 ]
+    },
+    "properties": {
+    "FIELD1":32,
+    "nuts2":"UKE3",
+    "country":"United Kingdom",
+    "production":0.15,
+    "id":"UKE3",
+    "na":"South Yorkshire",
+    "ar":1552609216,
+    "bubble_size":1
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 8.98,40.025 ]
+    },
+    "properties": {
+    "FIELD1":15,
+    "nuts2":"ITG2",
+    "country":"Italy",
+    "production":0.073,
+    "id":"ITG2",
+    "na":"Sardegna",
+    "ar":24115060288,
+    "bubble_size":1
+    }
+  }
+]
+}
